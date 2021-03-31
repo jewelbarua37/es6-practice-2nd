a02c7a30d156db5de0fd20fcf577d22ccbea9187
Jewel Barua <jewelbarua37@gmail.com>
2021-03-31 23:48:12 +0600

    letconst completed

---
+++ letconst.js
@@ -0,0 +1,11 @@
+const hubby = "Jewel Barua";
+console.log(hubby);
+
+
+const numbers =[12, 45, 20];
+numbers[3] = 88;
+numbers.push(63);
+console.log(numbers);
+
+
+const nayok = {name: "sakib khan", phone: 451252122}
